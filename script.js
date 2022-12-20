b561b085b97816a708b65879851fae43941f2bd2
PauloGerson <35962308+PauloGerson@users.noreply.github.com>
2022-12-20 01:11:21 -0300

    ajustando informações

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const citys = document.querySelector(".cidade")
 const input = document.querySelector("input");
 const btn = document.querySelector("button");
 const img = document.querySelector("img");
+const temp = document.querySelector(".temperatura")
 
 btn.addEventListener('click', function(){
     if(!input.value) return;
@@ -9,21 +10,19 @@ btn.addEventListener('click', function(){
     getTempo(input.value)
 })
 
-
-
-
-
 async function getTempo(city){
     const url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f987db7b0b1985697be1a3e2a371b858`)
     const response  = await url.json();
-   console.log(response)
+    console.log(response)
     getDados(response)
 
 }
 
 function getDados(dados){
-    citys.innerHTML = dados.name;
+    citys.innerHTML = `${dados.name}, ${dados.sys.country}` ;
     img.src = `http://openweathermap.org/img/wn/${dados.weather[0].icon}@2x.png`
+    let temp2 = JSON.stringify(dados.main.temp);
+    temp.innerHTML = `Temperatura: ${temp2.slice(0,2)}°C`
 
 }
     
