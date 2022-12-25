d204289efcf6bdecd742e3d9c42dd9a0a2f8e538
PauloGerson <35962308+PauloGerson@users.noreply.github.com>
2022-12-25 15:29:08 -0300

    editando readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Api-Clima-do-tempo
+Neste projeto é usado o localStorage para fazer um enviar de informações de uma tela para outra.
+
+# Tela 01<br>
+
+<img src="assets/img/tela1.png">
+
+# Tela 02 <br>
+
+<img src="assets/img/tela2.png">
--- assets/script/index.js
+++ assets/script/index.js
@@ -1,8 +1,8 @@
 const bntEnviar = document.querySelector('.btn');
 const input = document.querySelector('input')
+
 bntEnviar.addEventListener('click', function(){
-    if(!input.value) return;
-    
+    if(!input.value) return;   
     pegatempo(input.value)
     window.location.href= 'assets/pages/previsão.html'
 })
